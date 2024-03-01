9a28f5586028405b5aef3c1c639054aef81776a0
Jatin Chhabra <jatinc@maqsoftware.com>
2024-03-01 13:14:34 +0530

    udpated serialization options for exceptions

--- BookkeeperAPI/Middlewares/ExceptionHandler.cs
+++ BookkeeperAPI/Middlewares/ExceptionHandler.cs
@@ -37,7 +37,7 @@
             catch (Exception e)
             {
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await context.Response.WriteAsync(JsonSerializer.Serialize(new ErrorResponseModel { ErrorMessage = e.Message, StatusCode = StatusCodes.Status500InternalServerError }));
+                await context.Response.WriteAsync(JsonSerializer.Serialize(new ErrorResponseModel { ErrorMessage = e.Message, StatusCode = StatusCodes.Status500InternalServerError }, options));
                 _logger.LogError(e, e.Message);
                 return;
             }
