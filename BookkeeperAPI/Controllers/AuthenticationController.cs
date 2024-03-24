11845c90db18da720ceb0c23a2cc4a253f3743b7
Jatin Chhabra <57009264+Jatinchhabra21@users.noreply.github.com>
2024-03-24 17:27:03 +0530

    updated return type of Authentication controller (#23)

--- BookkeeperAPI/Controllers/AuthenticationController.cs
+++ BookkeeperAPI/Controllers/AuthenticationController.cs
@@ -1,90 +1,90 @@
-﻿namespace BookkeeperAPI.Controllers
+﻿namespace BookkeeperAPI.Controllers;
+
+#region usings
+
+using Exceptions;
+using Model;
+using Repository.Interface;
+using Utility;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+#endregion
+
+[ApiController]
+[AllowAnonymous]
+public class AuthenticationController : ControllerBase
 {
-    #region usings
-    using BookkeeperAPI.Entity;
-    using BookkeeperAPI.Exceptions;
-    using BookkeeperAPI.Model;
-    using BookkeeperAPI.Repository.Interface;
-    using BookkeeperAPI.Utility;
-    using Microsoft.AspNetCore.Authorization;
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.IdentityModel.Tokens;
-    using System.ComponentModel.DataAnnotations;
-    using System.IdentityModel.Tokens.Jwt;
-    using System.Security.Claims;
-    using System.Security.Cryptography;
-    using System.Text;
-    #endregion
+    private readonly IConfiguration _configuration;
+    private readonly IUserRepository _userRepository;
 
-    [ApiController]
-    [AllowAnonymous]
-    public class AuthenticationController : ControllerBase
+    public AuthenticationController(IConfiguration configuration, IUserRepository userRepository)
     {
-        private readonly IConfiguration _configuration;
-        private readonly IUserRepository _userRepository;
+        _configuration = configuration;
+        _userRepository = userRepository;
+    }
 
-        public AuthenticationController(IConfiguration configuration, IUserRepository userRepository)
+    [HttpPost("/api/oauth2/token")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthenticationResult))]
+    [ProducesErrorResponseType(typeof(ErrorResponseModel))]
+    public async Task<Ok<AuthenticationResult>> GetToken([FromBody] [Required] LoginCredential credential)
+    {
+        if (credential == null || credential.Email == null || credential.Password == null)
+            throw new HttpOperationException(400, "Bad Request");
+
+        var userInfo = await _userRepository.GetUserByEmailAsync(credential.Email);
+
+        if (userInfo == null) throw new HttpOperationException(400, "Invalid credentials");
+
+        if (!Parser.IsValidPassword(credential.Password, userInfo.Credential!.Password!))
+            throw new HttpOperationException(400, "Invalid credentials");
+
+        var claims = new[]
         {
-            _configuration = configuration;
-            _userRepository = userRepository;
-        }
+            new Claim(JwtRegisteredClaimNames.Sub, userInfo.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new Claim("user_id", userInfo.Id.ToString()),
+            new Claim("display_name", userInfo.Credential!.DisplayName!),
+            new Claim("user_name", userInfo.Credential.Email!),
+            new Claim("nbf", ToUnixEpoch(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64),
+            new Claim("iat", ToUnixEpoch(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64)
+        };
 
-        [HttpPost("/api/oauth2/token")]
-        [ProducesResponseType(StatusCodes.Status200OK, Type =typeof(JwtAccessToken))]
-        [ProducesErrorResponseType(typeof(ErrorResponseModel))]
-        public async Task<ActionResult<string>> GetToken([FromBody][Required] LoginCredential credential)
+        var expiresAt = DateTime.UtcNow.AddMinutes(30);
+        var rsa = RSA.Create();
+        rsa.FromXmlString(
+            Encoding.UTF8.GetString(Convert.FromBase64String(_configuration[_configuration["RSA:Key:Private"]!]!)));
+        var singIn = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
+        var token = new JwtSecurityToken(
+            _configuration[_configuration["Jwt:Iss"]!]!,
+            _configuration[_configuration["Jwt:Aud"]!]!,
+            claims,
+            signingCredentials: singIn,
+            expires: expiresAt
+        );
+
+        var accessToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+        return TypedResults.Ok(new AuthenticationResult()
         {
-            if (credential == null || credential.Email == null || credential.Password == null)
-            {
-                throw new HttpOperationException(400, "Bad Request");
-            }
+            AccessToken = accessToken,
+            ExpiresAt = expiresAt,
+            TokenId = Guid.NewGuid(),
+            UserName = userInfo.Credential.DisplayName!,
+            UserEmail = userInfo.Credential.Email!
+        });
+    }
 
-            User? userInfo = await _userRepository.GetUserByEmailAsync(credential.Email);
-
-            if (userInfo == null)
-            {
-                throw new HttpOperationException(400, "Invalid credentials");
-            }
-
-            if (!Parser.IsValidPassword(credential.Password, userInfo.Credential!.Password!))
-                throw new HttpOperationException(400, "Invalid credentials");
-
-            Claim[] claims = new[]
-            {
-                new Claim(JwtRegisteredClaimNames.Sub, userInfo.Id.ToString()),
-                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim("user_id", userInfo.Id.ToString()),
-                new Claim("display_name", userInfo.Credential!.DisplayName!),
-                new Claim("user_name", userInfo.Credential.Email!),
-                new Claim("nbf", ToUnixEpoch(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64),
-                new Claim("iat", ToUnixEpoch(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64)
-            };
-
-            DateTime expiresAt = DateTime.UtcNow.AddMinutes(30);
-            RSA rsa = RSA.Create();
-            rsa.FromXmlString(Encoding.UTF8.GetString(Convert.FromBase64String(_configuration[_configuration["RSA:Key:Private"]!]!)));
-            SigningCredentials singIn = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256);
-            JwtSecurityToken token = new JwtSecurityToken(
-                issuer: _configuration[_configuration["Jwt:Iss"]!]!,
-                audience: _configuration[_configuration["Jwt:Aud"]!]!,
-                claims: claims,
-                signingCredentials: singIn,
-                expires: expiresAt
-            );
-
-            string accessToken = new JwtSecurityTokenHandler().WriteToken(token);
-
-            return Ok(new JwtAccessToken()
-            {
-                AccessToken = accessToken,
-                ExpiresAt = expiresAt,
-                TokenId = Guid.NewGuid()
-            });
-        }
-
-        private static long ToUnixEpoch(DateTime date)
-        {   
-            return (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
-        }
+    private static long ToUnixEpoch(DateTime date)
+    {
+        return (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero))
+            .TotalSeconds);
     }
 }
---
+++ BookkeeperAPI/Model/AuthenticationResult.cs
@@ -0,0 +1,14 @@
+﻿namespace BookkeeperAPI.Model;
+
+public class AuthenticationResult
+{
+    public string? AccessToken { get; set; }
+
+    public DateTime ExpiresAt { get; set; }
+
+    public Guid TokenId { get; set; }
+
+    public string? UserName { get; set; }
+
+    public string? UserEmail { get; set; }
+}
